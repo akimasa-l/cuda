60183de77114c668fd29692454f57d80b4509669
Akimasa_L <liumingzheng8@outlook.com>
2021-08-11 12:27:24 +0900

    GPUとCPUで同じような演算ができた！！！

--- chapter01/hello.cu
+++ chapter01/hello.cu
@@ -4,10 +4,11 @@ __global__ void helloFromGPU() {
     printf("Hello World From GPU thread %d!\n", a);
 }
 int main() {
-    printf("Hello World From CPU!\n");
+    printf("Hello World From CPU1!\n");
     helloFromGPU<<<1, 10>>>();
-    printf("Hello World From CPU!\n");
-    // cudaDeviceReset();
-    cudaDeviceSynchronize();
+    printf("Hello World From CPU2!\n");
+    cudaDeviceReset();
+    // cudaDeviceSynchronize();
+    printf("Hello World From CPU3!\n");
     return 0;
 }
