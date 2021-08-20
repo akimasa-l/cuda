c892b4ee3f47427eee7250cbd36b8c61c7e1eb37
Akimasa_L <liumingzheng8@outlook.com>
2021-08-20 23:31:41 +0900

    時間めっちゃ減ってサンバみたいになったわ

--- work/newton_fractal/main.cu
+++ work/newton_fractal/main.cu
@@ -27,8 +27,8 @@ __device__ inline T get_place(const int ix, const int iy) {
 }
 
 __device__ inline T newton_method(T x) {
-    const T f = thrust::pow(x, 5) + T(1.0, 0.0);
-    const T df = 5 * thrust::pow(x, 4);
+    const T f = 1 / (1 + thrust::exp(x));
+    const T df = f * (1 - f);
     return df.real() == .0 && df.imag() == .0 ? df : x - f / df;
 }
 
