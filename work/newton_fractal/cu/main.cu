63e19b3e2fcfece8961f3a5802c4b0055de481c5
Akimasa_L <liumingzheng8@outlook.com>
2021-08-21 13:44:02 +0900

    本来のsigmoid

--- work/newton_fractal/cu/main.cu
+++ work/newton_fractal/cu/main.cu
@@ -27,9 +27,7 @@ __device__ inline T get_place(const int ix, const int iy) {
 }
 
 __device__ inline T newton_method(T x) {
-    // const T f = thrust::tanh(x);
-    // const T df = 1 / thrust::pow(thrust::cosh(x), 2);
-    const T f = 1 / (1 + thrust::exp(x));
+    const T f = 1 / (1 + thrust::exp(-x));
     const T df = f * (1 - f);
     return df.real() == .0 && df.imag() == .0 ? df : x - f / df;
 }
