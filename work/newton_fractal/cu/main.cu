a40e528b496dedd7465b7650e492b658135d61c6
Akimasa_L <liumingzheng8@outlook.com>
2021-08-21 12:39:33 +0900

    logisticnにしてもおもしろくない

--- work/newton_fractal/cu/main.cu
+++ work/newton_fractal/cu/main.cu
@@ -27,8 +27,8 @@ __device__ inline T get_place(const int ix, const int iy) {
 }
 
 __device__ inline T newton_method(T x) {
-    const T f = thrust::pow(1 - thrust::pow(x, 2), 0.5);
-    const T df = -x / f;
+    const T f = 2 * thrust::pow(x * (1 - x), 0.5);
+    const T df = (1 - 2 * x) / thrust::pow(x - x * x, 0.5);
     // const T f = 1 / (1 + thrust::exp(x));
     // const T df = f * (1 - f);
     return df.real() == .0 && df.imag() == .0 ? df : x - f / df;
