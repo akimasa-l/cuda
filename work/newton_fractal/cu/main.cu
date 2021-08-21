bb2758d90c4b044b7a5679996f7af95703f7f1ad
Akimasa_L <liumingzheng8@outlook.com>
2021-08-21 14:31:38 +0900

    しっぱい

--- work/newton_fractal/cu/main.cu
+++ work/newton_fractal/cu/main.cu
@@ -5,9 +5,9 @@ using F = double;
 using T = thrust::complex<F>;
 
 constexpr F range_x_max = +10;
-constexpr F range_x_min = -0;
-constexpr F range_y_max = +5;
-constexpr F range_y_min = -5;
+constexpr F range_x_min = -10;
+constexpr F range_y_max = +10;
+constexpr F range_y_min = -10;
 constexpr int block_x = 256;
 constexpr int block_y = 256;
 constexpr int thread_x = 32;
@@ -27,8 +27,8 @@ __device__ inline T get_place(const int ix, const int iy) {
 }
 
 __device__ inline T newton_method(T x) {
-    const T f = x * thrust::atan(x) + thrust::log(thrust::pow(x, 2) - 1) / 2;
-    const T df = thrust::atan(x);
+    const T f = thrust::pow(x,2)*thrust::pow(thrust::sin(x),3);
+    const T df = x*thrust::pow(thrust::sin(x),2)*(2*thrust::sin(x)+3*x*thrust::cos(x));
     return df.real() == .0 && df.imag() == .0 ? df : x - f / df;
 }
 
