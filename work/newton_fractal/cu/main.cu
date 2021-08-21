da424207c971cc4d7d4f01d26cf438476541e110
Akimasa_L <liumingzheng8@outlook.com>
2021-08-21 15:59:03 +0900

    スロベニア

--- work/newton_fractal/cu/main.cu
+++ work/newton_fractal/cu/main.cu
@@ -28,7 +28,7 @@ __device__ inline T get_place(const int ix, const int iy) {
 
 __device__ inline T newton_method(T x) {
     const T f = 1 / (1 + thrust::exp(x));
-    const T df = f * (-1 - f);
+    const T df = f * (-1 + f);
     return df.real() == .0 && df.imag() == .0 ? df : x - f / df;
 }
 
